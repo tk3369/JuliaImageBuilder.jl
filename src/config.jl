d855d0980450234e715f0efbe2e308ca56033df5
Tom Kwong <tk3369@gmail.com>
2021-09-19 12:50:40 -0700

    Update plots, add comments

--- src/config.jl
+++ src/config.jl
@@ -1,7 +1,10 @@
 CONFIGS = [
+    # JuliaFormatter.jl
     SysImageConfig("julia-formatter", [Dependency(:JuliaFormatter, nothing)]),
+
+    # Plots.jl
     SysImageConfig(
-        "julia-plot",
+        "julia-plots",
         [Dependency(:Plots, "deps/SnoopCompile/precompile_script.jl")],
     ),
 ]
